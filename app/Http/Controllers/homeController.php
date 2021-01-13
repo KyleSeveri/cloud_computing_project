b819e88a9eb2c1c3ed0d32d2348a5257ab2919ce
Florian <44845546+FlorianCoenen@users.noreply.github.com>
2021-01-13 21:16:11 +0100

    SOAP_QuoteProvider

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -8,6 +8,17 @@ class homeController extends Controller
 {
     public function goMovie()
     {
+        $sw = new SoapWrapper();
+        $sw->add("QuoteProvider",  function ($service) {
+        $service->wsdl("http://localhost:55112/QuoteProvider.asmx?WSDL") 
+                   ->trace(true)
+                   ->classmap([
+                   QuoteOfTheDay::class
+                   ]);
+        });
+        // vanaf hier gebruiken we de SOAP wrapper
+        $response = $sw->call("QuoteProvider.GetSnack", [new QuoteOfTheDay("Action")]);
+        $quote = $response->GetSnackResult;
         return view('movie');
     }
 }
